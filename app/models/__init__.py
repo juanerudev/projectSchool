e38c0ab5f2a9abdc40c3543eafbadcefc6cc4430
Juan Rodriguez <juanerudev@gmail.com>
2025-07-23 11:32:59 -0500

    Se hizo un pequeño ajuste en el __init__.py y al endpoint de crear profesor se valido que no se pueda crear si el email ya existe o si el departamento no existe

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
 from .departamento_model import Departamento, CrearDepartamento, ActualizarDepartamento
 from .profesor_model import Profesor, CrearProfesor, ActualizarProfesor
 
-__all__ = ["Departamento", "CrearDepartamento", "ActualizarDepartamento", "Profesor", "CrearDepartamento", "ActualizarProfesor"]
+__all__ = ["Departamento", "CrearDepartamento", "ActualizarDepartamento", "Profesor", "CrearProfesor", "ActualizarProfesor"]
--- app/routers/profesor_router.py
+++ app/routers/profesor_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, status
-from models import Profesor, CrearProfesor, ActualizarProfesor
+from models import Profesor, CrearProfesor, ActualizarProfesor, Departamento
 from db import SessionDep
 from sqlmodel import select
 
@@ -7,6 +7,16 @@ router = APIRouter()
 
 @router.post("/", response_model=Profesor, status_code=status.HTTP_201_CREATED)
 async def create_profesor(session: SessionDep, profesor_data: CrearProfesor):
+    existe_email = session.exec(select(Profesor).where(Profesor.email == profesor_data.email)).first()
+
+    if existe_email:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Ya existe un profesor con ese email")
+    
+    departamento = session.get(Departamento, profesor_data.departamento_id)
+
+    if not departamento:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="El departamento especificado no existe")
+
     profesor_dict = profesor_data.model_dump()
     profesor_dict['nombre'] = profesor_dict['nombre'].strip()
     profesor = Profesor.model_validate(profesor_dict)
